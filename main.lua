0ff200dbd611f45dedb762ba9c7d775e2f69c481
Kevin Bergin <bergin.kevin@gmail.com>
2013-05-05 02:10:42 +0100

    Add surrounding tile interaction

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
-require("loveps3")
+require('loveps3')
+require('tween')
 
 -- ********* STATE *********
+
+MENU = 0
+PLAYING = 1
+PAUSE = 2
+NO_CONTROLLER = 3
+
+state = MENU
 
 -- Grid items
 local EMPTY = 0
@@ -16,16 +24,14 @@ local TALKING = 2
 -- Player directions
 local LEFT = 1
 local RIGHT = 2
-local BEHIND = 3
-local IN_FRONT = 4
+local UP = 3
+local DOWN = 4
 
 -- Worker state
-local UNCONVERTED = 1
-local CONVERTING = 2
-local CONVERTED = 3
+local CONVERTING = 1
+local CONVERTED = 2
 
 -- Current states
-local playerState = NONE
 local playerTarget = NONE
 local workerState = UNCONVERTED
 
@@ -33,34 +39,133 @@ local workerState = UNCONVERTED
 local currentLevel = 1
 local warnings = 0
 local renderGrid = nil
+local playerTile = nil
 
 
 -- ********* LOVE FUNCTIONS *********
 
 function love.load()
-    controller = getController(1, buttonListener, nil)
-    renderGrid = newGrid(LEVELS[1], 32)
+    controller = getController(1, buttonListener, stickListener)
+    if controller == nil then
+        state = NO_CONTROLLER
+        return
+    end
+    state = PLAY
+    renderGrid = newGrid(LEVELS[currentLevel].grid, 32)
 end
 
 function love.update(dt)    
-    controller:update()
+    if state == PLAY then
+        controller:update(false)
+        updateWorkers()
+    end
 end
 
 function love.draw()
-    renderGrid:draw(50, 50)
+    if state == PLAY then
+        renderGrid:draw(LEVELS[currentLevel].x, LEVELS[currentLevel].y)
+    elseif state == NO_CONTROLLER then
+        love.graphics.print("NO CONTROLLER FOUND!", 100, 100)
+    end
 end
 
 -- ********* CONTROLS *********
 
-function buttonListener()
+function buttonListener(button)
 
+end
+
+function stickListener(stick, vector)
+    if stick == 'LEFT' then
+        if pointingLeft(vector) then
+            talkLeft()
+        elseif pointingRight(vector) then
+            talkRight()
+        elseif pointingUp(vector) then
+            talkUp()
+        elseif pointingDown(vector) then
+            talkDown()
+        else
+            resetPlayer()
+        end
+    end
+end
+
+function resetPlayer()
+    playerTile.state = NONE
+    playerTarget = NONE
+end
+
+function talkLeft()
+    playerTile.state = TALKING
+    playerTarget = LEFT
+end
+
+function talkRight()
+    playerTile.state = TALKING
+    playerTarget = RIGHT
+end
+
+function talkUp()
+    playerTile.state = TALKING
+    playerTarget = UP
+end
+
+function talkDown()
+    playerTile.state = TALKING
+    playerTarget = DOWN
+end
+
+-- GAME LOGIC
+
+function updateWorkers()
+    for i=1, table.getn(renderGrid.data) do
+        for j=1, table.getn(renderGrid.data[i]) do
+            item = renderGrid.data[i][j]
+            if item.type == WORKER then
+                if item.state == NONE and talking(item) then
+                    item.state = CONVERTING
+                    break
+                else
+                    if item.state == CONVERTING then
+                        item.state = NONE
+                        if item.tween ~= nil then
+                            tween.stop(item.tween)
+                            item.tween = nil
+                            item.state = NONE
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function talking(worker)
+    if playerTile.state == TALKING then
+        if isTarget(LEFT, worker) or 
+            isTarget(RIGHT, worker) or
+            isTarget(UP, worker) or
+            isTarget(DOWN, worker) then
+            return true
+        end
+    end
+    return false
+end
+
+function isTarget(direction, worker)
+    return playerTarget == LEFT and  worker == getPlayerRelativeTile(-1, 0) or
+        playerTarget == RIGHT and  worker == getPlayerRelativeTile(1, 0) or
+        playerTarget == UP and  worker == getPlayerRelativeTile(0, -1) or
+        playerTarget == DOWN and  worker == getPlayerRelativeTile(0, 1)
+end
+
+function getPlayerRelativeTile(xOffset, yOffset)
+    return renderGrid.data[playerTile.localX + xOffset][playerTile.localY + yOffset]
 end
 
 -- ********* GRID ********* 
 
-function drawGrid()
-    
-end
 
 local grid = {}
 grid.__index = grid
@@ -77,7 +182,11 @@ function loadGrid(level)
     for i=1, table.getn(level) do
         parsedGrid[i] = {}
         for j=1, table.getn(level[i]) do
-            table.insert(parsedGrid[i], newGridItem(i, j, level[i][j], NONE))
+            item = newGridItem(i, j, level[i][j])
+            if item.type == PLAYER then
+                playerTile = item
+            end
+            table.insert(parsedGrid[i], item)
         end
     end
     return parsedGrid
@@ -88,7 +197,7 @@ function grid:draw(x, y)
         for j=1, table.getn(self.data[i]) do
             tileX = x + (TILE_SIZE * (i - 1))
             tileY = y + (TILE_SIZE * (j - 1))
-            self.data[i][j].draw(tileX, tileY)
+            self.data[i][j].draw(tileX, tileY, self.data[i][j])
         end
     end
 end
@@ -103,7 +212,7 @@ function newGridItem(localX, localY, type, state)
     gi.localX = localX
     gi.localY = localY
     gi.type = type
-    gi.state = state
+    gi.state = NONE
     if gi.type == WORKER then
         gi.draw = drawWorker
     elseif gi.type == PLAYER then
@@ -116,22 +225,34 @@ function newGridItem(localX, localY, type, state)
     return setmetatable(gi, gridItem)
 end
 
-function drawWorker(x, y)
+function drawWorker(x, y, item)
     love.graphics.setColor(0, 255, 0)
     love.graphics.rectangle('fill', x, y, TILE_SIZE - 5, TILE_SIZE - 5)
+
+    if item.state == CONVERTING then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.print('C', x + 5, y + 10)
+    end
 end
 
-function drawPlayer(x, y)
+function drawPlayer(x, y, item)
     love.graphics.setColor(255, 0, 0)
     love.graphics.rectangle('fill', x, y, TILE_SIZE - 5, TILE_SIZE - 5)
+    love.graphics.setColor(0, 0, 0)
+    if item.state == TALKING then
+        love.graphics.print('T', x + 5, y + 10)
+    else
+        love.graphics.print('P1', x + 5, y + 10)
+    end
+    
 end
 
-function drawSupervisor(x, y)
+function drawSupervisor(x, y, item)
     love.graphics.setColor(0, 0, 255)
     love.graphics.rectangle('fill', x, y, TILE_SIZE - 5, TILE_SIZE - 5)
 end
 
-function drawSpace(x, y)
+function drawSpace(x, y, item)
     love.graphics.setColor(255, 255, 255)
     love.graphics.rectangle('fill', x, y, TILE_SIZE - 5, TILE_SIZE - 5)
 end
@@ -146,11 +267,14 @@ TILE_SIZE = 64
 -- 3: Supervisor starting point
 LEVELS = {
     {
-        {SUPERVISOR, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-        {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
-        {EMPTY, WORKER, PLAYER, WORKER, WORKER, EMPTY},
-        {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
-        {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
-        {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}
+        x = 200, y = 100,
+        grid={
+            {SUPERVISOR, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
+            {EMPTY, WORKER, PLAYER, WORKER, WORKER, EMPTY},
+            {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
+            {EMPTY, WORKER, WORKER, WORKER, WORKER, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}
+        }
     }
 }
